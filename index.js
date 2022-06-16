a0481e8e126a282446a9a6ec1c3e86368c725213
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-17 01:14:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email){
+  
+  const configurationObject = {
+   method: "POST",
+   headers: {
+     "Content-Type": "application/json",
+     Accept:"application/json",
+   },
+   body: JSON.stringify({
+    name: `${name}`,
+    email: `${email}`,
+   })
+  }
+   fetch("http://localhost:3000/users", configurationObject)
+   .then(function(response){
+     return response.json()})
+    .then(function(object){
+      
+      const body = document.querySelector("body")
+body.textContent = object.id
+})
+.catch(error){
+  body.innerHTML = error.message
+}
+
+
+
+}
